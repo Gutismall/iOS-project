2cdd8882519728c25d5a5ddaef0099f45108857e
Gutismall <ari.guter@gmail.com>
2025-08-22 04:50:30 +0300

    created change budget modal

---
+++ iOS project/Components/ChnageBudgetModal.swift
@@ -0,0 +1,20 @@
+//
+//  ChnageBudgetModal.swift
+//  iOS project
+//
+//  Created by Ari Guterman on 22/08/2025.
+//
+
+import UIKit
+
+class ChnageBudgetModal: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ iOS project/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  iOS project
+//
+//  Created by Ari Guterman on 21/08/2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
